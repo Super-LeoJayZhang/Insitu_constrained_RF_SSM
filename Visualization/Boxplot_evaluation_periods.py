e4e188023754950d689b5c4f0a095ee7c5769867
L.Zhang (Windows) <leojayak@gmail.com>
2021-09-19 15:47:56 +0200

    fixed a typo in the figure 'Pearson correlation'

--- Visualization/Boxplot_evaluation_periods.py
+++ Visualization/Boxplot_evaluation_periods.py
@@ -17,9 +17,9 @@ file_metrics = r'C:\Users\leojay\Desktop\ESSD-Long Term SSM\Insitu_constrained_R
 df_metrics = pd.read_csv(file_metrics, index_col=0)
 # Rename the columns name.
 df_metrics.columns = ['network', 'station', 'NOP',
-                      'RMSE: RF SSM', 'ubRMSE: RF SSM', 'Person Correlation: RF SSM',
+                      'RMSE: RF SSM', 'ubRMSE: RF SSM', 'Pearson Correlation: RF SSM',
                       'LandCover', 'Climate',
-                      'RMSE: ESA-CCI SSM', 'ubRMSE: ESA-CCI SSM', 'Person Correlation: ESA-CCI SSM']
+                      'RMSE: ESA-CCI SSM', 'ubRMSE: ESA-CCI SSM', 'Pearson Correlation: ESA-CCI SSM']
 
 fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 8))
 axs[0] = sns.boxplot(ax=axs[0], data=df_metrics,
@@ -30,11 +30,11 @@ axs[0] = sns.boxplot(ax=axs[0], data=df_metrics,
 
 axs[0].set_ylabel(ylabel='Surface Soil Moisture [$\mathregular{cm^3}$/$\mathregular{cm^3}$]', fontsize=12)
 axs[0].set_title(r'(a) Boxplot of RMSE and ubRMSE', fontsize=12)
-axs[1] = sns.boxplot(ax=axs[1], data=df_metrics, order=['Person Correlation: RF SSM', 'Person Correlation: ESA-CCI SSM'],
+axs[1] = sns.boxplot(ax=axs[1], data=df_metrics, order=['Pearson Correlation: RF SSM', 'Pearson Correlation: ESA-CCI SSM'],
                      palette=[sns.color_palette("hls", 8)[5], sns.color_palette("hls", 8)[1]],
                      width=0.3)
 axs[1].set_ylabel(ylabel='Person Correlation (r)', fontsize=12)
-axs[1].set_title(r'(b) Boxplot of Person Correlation (r)')
+axs[1].set_title(r'(b) Boxplot of Pearson Correlation (r)')
 
 plt.savefig(r'C:\Users\leojay\Desktop\ESSD-Long Term SSM\ESSD_manuscript\Scientific_papers\Figures\Boxplot_evaluation.jpg'
             , dpi=300)
